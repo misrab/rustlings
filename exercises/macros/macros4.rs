1112a725c3baa6e2b68059e9e811e207a212f925
Misrab <faizullah.misrab@gmail.com>
2019-10-19 18:23:58 +0800

    finish macros and test 4

--- exercises/macros/macros4.rs
+++ exercises/macros/macros4.rs
@@ -4,7 +4,8 @@
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
-    }
+    }; 
+    
     ($val:expr) => {
         println!("Look at this other macro: {}", $val);
     }
